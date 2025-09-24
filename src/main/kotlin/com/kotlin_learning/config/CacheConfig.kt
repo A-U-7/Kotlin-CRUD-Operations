73c317b86b795636f992c6e34ccac0ef6c5aaac0
A-U-7 <A-U-7@users.noreply.github.com>
2025-09-24 20:25:05 +0530

    Redis Caching Configurations

---
+++ src/main/kotlin/com/kotlin_learning/config/CacheConfig.kt
@@ -0,0 +1,28 @@
+package com.kotlin_learning.config
+
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import java.time.Duration
+
+@Configuration
+@EnableCaching
+class CacheConfig {
+
+    @Bean
+    fun cacheManager(redisConnectionFactory: RedisConnectionFactory): CacheManager {
+        val cacheConfig = RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofMinutes(30)) // Default TTL
+            .disableCachingNullValues()
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .withCacheConfiguration("users", cacheConfig.entryTtl(Duration.ofMinutes(30)))
+            .withCacheConfiguration("user", cacheConfig.entryTtl(Duration.ofMinutes(10)))
+            .cacheDefaults(cacheConfig)
+            .build()
+    }
+}
